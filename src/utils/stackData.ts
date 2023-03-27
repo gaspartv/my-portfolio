bf460eafda341e5da0e1786fa75c6c97547073f5
= <cadastro@diegogaspar.dev.br>
2023-03-27 16:04:04 -0300

    Feat start project

---
+++ src/utils/stackData.ts
@@ -0,0 +1,59 @@
+import { TbBrandNextjs } from "react-icons/tb";
+import {
+  SiPostgresql,
+  SiTypescript,
+  SiTailwindcss,
+  SiMongodb,
+  SiJest,
+  SiPrisma,
+  SiRedux,
+} from "react-icons/si";
+import {
+  FaDocker,
+  FaHtml5,
+  FaCss3Alt,
+  FaJs,
+  FaNode,
+  FaReact,
+  FaGit,
+  FaPython,
+} from "react-icons/fa";
+import { DiDjango } from "react-icons/di";
+
+export const stackData = [
+  {
+    title: "HTML",
+    img: FaHtml5,
+  },
+  {
+    title: "CSS",
+    img: FaCss3Alt,
+  },
+  {
+    title: "Tailwindcss",
+    img: SiTailwindcss,
+  },
+  {
+    title: "JS",
+    img: FaJs,
+  },
+  { title: "TypeScript", img: SiTypescript },
+  {
+    title: "Node JS",
+    img: FaNode,
+  },
+  {
+    title: "React",
+    img: FaReact,
+  },
+  { title: "Next.js", img: TbBrandNextjs },
+  { title: "Docker", img: FaDocker },
+  { title: "PostgreSQL", img: SiPostgresql },
+  { title: "Mongodb", img: SiMongodb },
+  { title: "Git", img: FaGit },
+  { title: "Python", img: FaPython },
+  { title: "Django", img: DiDjango },
+  { title: "Jest", img: SiJest },
+  { title: "Prisma", img: SiPrisma },
+  { title: "Redux", img: SiRedux },
+];
---
+++ src/utils/userData.ts
@@ -0,0 +1,9 @@
+export const userData = {
+  nameUser: "Diego Monteiro",
+  githubUser: "gaspartv",
+  linkedinUser: "devdiegogaspar",
+  whatsappNumber: "+5532998274714",
+  emailUser: "contato@diegogaspar.dev.br",
+  instagramUser: "gaspardbobs",
+  facebookUser: "",
+};
